86972bea061c3087224453fc49c8afc9378910d4
Marta_kachanova <marta.kachanova@gmail.com>
2023-07-04 15:46:18 +0300

    new project

---
+++ online_shop/src/components/Header.js
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <header class="header">
+      <div className="container">
+        <div className="header__container">
+          <Link to="/">
+            <img src="images/logo.png" alt="logo" height="45" />
+          </Link>
+          <div className="header__info">
+            Hi,{" "}
+            <Link to="/account" className="header__user">
+              Log in
+            </Link>
+            <div className="header__shop">
+              <Link to="/favourites">
+                <img src="images/favourite.png" alt="favourite" height="20" />
+                <span
+                  className="header__shop--count"
+                  id="headerFavouritesCount"
+                >
+                  0
+                </span>
+              </Link>
+            </div>
+            <button className="header__logout" id="headerLogout">
+              Log out
+            </button>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
---
+++ online_shop/src/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import Account from "./components/Account";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ItemsList from "./components/ItemsList";
+import FavouritesList from "./components/FavouritesList";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <ItemsList />,
+      },
+      {
+        path: "account",
+        element: <Account />,
+      },
+      {
+        path: "favourites",
+        element: <FavouritesList />,
+      },
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<RouterProvider router={router} />);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
